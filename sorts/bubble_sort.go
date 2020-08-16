776b5051f3d08fc9ea283cd960e4bffdb3e96663
Guillaume <guillaume.rochedix@gmail.com>
2020-08-16 18:28:37 +0200

    improve bubble sort algo

--- sorts/bubble_sort.go
+++ sorts/bubble_sort.go
@@ -1,12 +1,11 @@
 package sorts
 
 func bubbleSort(collection []int) []int {
-	var swapped bool = true
-	for i := 1; i < len(collection); i++ {
-		swapped = false
-		for j := 1; j < len(collection); {
-			if collection[j-1] > collection[j] {
-				collection[j], collection[j-1] = collection[j-1], collection[j]
+	for i := 0; i < len(collection); i++ {
+		swapped := false
+		for j := 0; j < len(collection)-i-1; j++ {
+			if collection[j+1] < collection[j] {
+				collection[j], collection[j+1] = collection[j+1], collection[j]
 				swapped = true
 			}
 		}
