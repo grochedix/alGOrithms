2ff0295f6abdc35fc31ffb080067234eaa94c643
Guillaume <guillaume.rochedix@gmail.com>
2020-09-10 22:41:03 +0200

    jump search

---
+++ search/jump_search.go
@@ -0,0 +1,23 @@
+package search
+
+import "math"
+
+func jumpSearch(arr []int, x int) int {
+	n := float64(len(arr))
+	step := math.Floor(math.Sqrt(n))
+	for arr[int(math.Min(step, n-1))] < x {
+		if step >= n {
+			return -1
+		}
+		step += math.Floor(math.Sqrt(n))
+	}
+	for pos := int(math.Min(step, n-1)); arr[pos] >= x; pos-- {
+		if arr[pos] == x {
+			return pos
+		}
+		if pos == int(step-math.Floor(math.Sqrt(n))) {
+			return -1
+		}
+	}
+	return -1
+}
--- search/search_test.go
+++ search/search_test.go
@@ -13,5 +13,6 @@ func TestSearch(t *testing.T) {
 	collection = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 	for i := range collection {
 		fmt.Println(strconv.Itoa(i) + " " + strconv.Itoa(binarySearch(collection, i)))
+		fmt.Println(strconv.Itoa(i) + " " + strconv.Itoa(jumpSearch(collection, i)))
 	}
 }
